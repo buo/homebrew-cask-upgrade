9f0ef34f5900cf86c1fb9dc253f1dc836aec9eb4
buo <buo@users.noreply.github.com>
2016-06-26 11:21:17 +0900

    Use caskroom path defined in Hbc

--- lib/brew-cask-upgrade.rb
+++ lib/brew-cask-upgrade.rb
@@ -5,7 +5,7 @@ $LOAD_PATH.unshift("#{CASK_HOME}/lib")
 require 'vendor/homebrew-fork/global'
 require 'hbc'
 
-CASKROOM = "/usr/local/Caskroom"
+CASKROOM = Hbc.caskroom
 
 def each_installed
   Hbc.installed.each_with_index do |name, i|
