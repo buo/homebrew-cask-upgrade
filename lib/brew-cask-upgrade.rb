3850af7e51f5e3d46a0510ea6af17cd6818a51e1
buo <buo@users.noreply.github.com>
2016-07-05 22:37:14 +0900

    Add Homebrew directory to $LOAD_PATH
    fix #5

--- lib/brew-cask-upgrade.rb
+++ lib/brew-cask-upgrade.rb
@@ -1,6 +1,7 @@
 BREW_HOME = `brew --repository`.strip
 CASK_HOME = "#{BREW_HOME}/Library/Taps/caskroom/homebrew-cask"
 $LOAD_PATH.unshift("#{CASK_HOME}/lib")
+$LOAD_PATH.unshift(File.expand_path("#{BREW_HOME}/Library/Homebrew"))
 
 require 'vendor/homebrew-fork/global'
 require 'hbc'
