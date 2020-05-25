f61ca8c408de4f758c7d28e0db687126795c1b30
Ondrej Führer <ondrejfuhrer@users.noreply.github.com>
2020-05-25 17:26:34 +0200

    Removing hyphen as well from short version string

---
+++ lib/extend/version.rb
@@ -0,0 +1,17 @@
+module Cask
+  class DSL
+    class Version
+      def before_hyphen
+        version { split("-", 2).first }
+      end
+
+      def after_hyphen
+        version { split("-", 2).second }
+      end
+
+      def before_separators
+        version { before_hyphen.before_colon.before_comma }
+      end
+    end
+  end
+end
